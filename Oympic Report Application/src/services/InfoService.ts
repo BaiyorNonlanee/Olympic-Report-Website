becd5e6f3b280bfe39692e2d6870c5ab0132c935
Nonlanee <nonneenie@gmail.com>
2024-10-19 22:13:04 +0700

    change db

--- Oympic Report Application/src/services/InfoService.ts
+++ Oympic Report Application/src/services/InfoService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/BaiyorNonlanee/olympicDB',
+  baseURL: 'import.meta.env.VITE_BACKEND_URL',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
