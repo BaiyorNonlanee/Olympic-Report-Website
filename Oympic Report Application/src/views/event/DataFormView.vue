365aacfe4b86039f32fba83dcf386c61475fb206
Nichapat <Nichapat_Padung@cmu.ac.th>
2024-10-20 23:06:51 +0700

    update style

--- Oympic Report Application/src/views/event/DataFormView.vue
+++ Oympic Report Application/src/views/event/DataFormView.vue
@@ -50,9 +50,19 @@ onMounted(() => {
 })
 </script>
 <template>
-    <h1 class="text-center text-2xl font-bold mb-4 " style="margin-top: 5% ; margin-bottom: 2%;" >Update Information</h1>
-    <div class="container mx-auto p-4">
-      <form @submit.prevent="saveCountry" class="w-full h-auto bg-white p-6 rounded shadow-md">
+    <nav class="navbar bg-black p-4 flex items-center justify-center">
+      <img
+        src="@/assets/logo-2.png"
+        alt="Logo"
+        class="w-[120px] h-[120px] md:w-[150px] md:h-[150px]"
+      />
+    </nav>
+    <h1 class="text-center text-3xl font-bold mb-4" style="margin-top: 3%; color: #0d3b66;">Update Information</h1>
+    <div class="container mx-auto p-4" style="background-color: beige;">
+      <form 
+      @submit.prevent="saveCountry" 
+    class="w-full h-auto p-6 rounded relative rounded-xl rounded p-2">
+        
         <div class="form-group mb-4 flex items-center">
           <label for="countryName" class="block mb-5 w-1/3">Country Name:</label>
           <BaseInput 
@@ -60,7 +70,7 @@ onMounted(() => {
             type="text" 
             id="countryName" 
             placeholder="Country Name" 
-            class="w-1/3 border border-gray-300 rounded p-2 resize rounded-xl p-2"
+            class="w-1/3 border border-gray-300 rounded p-2 resize rounded-xl"
             style="min-height: 40px; resize: vertical;"
           />
         </div>
@@ -78,82 +88,84 @@ onMounted(() => {
         </div>
   
         <div class="form-group mb-4">
-        <label class="block mb-5">Medals:</label>
-        <div class="flex space-x-4">
-          <div class="flex-1 flex items-center" style="margin-left: 10%;">
-            <label for="gold" class="block mb-5 mr-2">Gold:</label>
-            <BaseInput 
-              v-model="country.gold" 
-              type="text" 
-              id="gold" 
-              placeholder="Gold" 
-              class="w-1/3 border border-gray-300 rounded p-2 rounded-xl"
-            />
-          </div>
-          <div class="flex-1 flex items-center">
-            <label for="silver" class="block mb-5 mr-2">Silver:</label>
-            <BaseInput 
-              v-model="country.silver" 
-              type="text" 
-              id="silver" 
-              placeholder="Silver" 
-              class="w-1/3 border border-gray-300 rounded p-2 rounded-xl"
-            />
-          </div>
-          <div class="flex-1 flex items-center">
-            <label for="bronze" class="block mb-5 mr-2">Bronze:</label>
-            <BaseInput 
-              v-model="country.bronze" 
-              type="text" 
-              id="bronze" 
-              placeholder="Bronze" 
-              class="w-1/3 border border-gray-300 rounded p-2 rounded-xl"
-            />
+          <label class="block mb-5">Medals:</label>
+          <div class="flex space-x-4">
+            <div class="flex-1 flex items-center" style="margin-left: 10%;">
+              <label for="gold" class="block mb-5 mr-2">Gold:</label>
+              <img 
+                    src="@/assets/gold.png" 
+                    alt="Bronze Medal" 
+                    class="w-6 h-10 mb-3 mr-3 ml-1">
+              <BaseInput 
+                v-model="country.gold" 
+                type="text" 
+                id="gold" 
+                placeholder="Gold" 
+                class="w-1/3 border border-gray-300 rounded p-2 rounded-xl"
+              />
+            </div>
+            <div class="flex-1 flex items-center">
+              <label for="silver" class="block mb-5 mr-2">Silver:</label>
+              <img 
+                    src="@/assets/silver.png" 
+                    alt="Bronze Medal" 
+                    class="w12 h-10 mb-3">
+              <BaseInput 
+                v-model="country.silver" 
+                type="text" 
+                id="silver" 
+                placeholder="Silver" 
+                class="w-1/3 border border-gray-300 rounded p-2 rounded-xl"
+              />
+            </div>
+            <div class="flex-1 flex items-center">
+                <label for="bronze" class="block mb-5 mr-2">Bronze:</label>
+                <img 
+                    src="@/assets/bronze.png" 
+                    alt="Bronze Medal" 
+                    class="w-12 h-10 mb-3">
+                <BaseInput 
+                    v-model="country.bronze" 
+                    type="text" 
+                    id="bronze" 
+                    placeholder="Bronze" 
+                    class="w-1/3 border border-gray-300 rounded p-2 rounded-xl"
+                />
+            </div>
           </div>
         </div>
-      </div>
   
-      <div class="form-group mb-4 flex items-center">
-        <label for="sport" class="block mb-5 w-1/3">Sport:</label>
-        <select 
-            v-model="country.sports.id" 
-            id="sport" 
-            class="w-1/3 border border-gray-300 rounded-xl p-2 custom-dropdown"
-        >
-            <option v-for="sport in sports" :key="sport.id" :value="sport.id">
-            {{ sport.name }}
-            </option>
-        </select>
+        <div class="form-group mb-4 flex items-center">
+          <label for="sport" class="block mb-5 w-1/3">Sport:</label>
+          <select 
+              v-model="country.sports.id" 
+              id="sport" 
+              class="w-1/3 border border-gray-300 rounded-xl p-2 custom-dropdown"
+          >
+              <option v-for="sport in sports" :key="sport.id" :value="sport.id">
+              {{ sport.name }}
+              </option>
+          </select>
         </div>
   
         <div class="form-group mb-4 flex items-center">
           <label for="image" class="block mb-5 w-1/3">Upload Image:</label>
           <input 
-            type="file" 
-            id="image" 
-            class="w-2/3 border border-gray-300 rounded p-2 rounded-xl"
-            @change="onFileChange"
-          />
+              type="file" 
+              id="image" 
+              class="w-2/3 border border-gray-300 rounded p-2 rounded-xl"
+              @change="onFileChange"
+              style="min-height: 120px; margin-top: 1.5%;">
         </div>
   
         <button 
-          class="submit-button w-full py-2 text-white bg-blue-600 rounded hover:bg-blue-700 transition" 
-          type="submit"
-        >
-          Submit
+            class="submit-button w-full py-2 text-white bg-red-600 rounded hover:bg-red-700 transition rounded-xl" style="margin-top: 2%;"  
+            type="submit">
+            Submit
         </button>
       </form>
     </div>
   </template>
   
 <style scoped>
-.custom-dropdown {
-  background-color: white; /* สีพื้นหลังปกติ */
-  transition: background-color 0.3s ease; /* เพื่อให้การเปลี่ยนสีลื่นไหล */
-}
-
-.custom-dropdown:hover {
-  background-color: #678eb4; /* สีที่กำหนดเมื่อ hover */
-  color: rgb(0, 0, 0); /* สีข้อความเมื่อ hover */
-}
 </style>
